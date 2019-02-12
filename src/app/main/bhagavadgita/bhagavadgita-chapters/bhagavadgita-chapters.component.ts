3b4d9824296c6382e4196aefc34818797f12f88c
arjunanatwork <arjunanatwork@gmail.com>
2019-02-12 22:23:38 +0400

    Adding Compatibilty to SCSS

--- src/app/main/bhagavadgita/bhagavadgita-chapters/bhagavadgita-chapters.component.ts
+++ src/app/main/bhagavadgita/bhagavadgita-chapters/bhagavadgita-chapters.component.ts
@@ -3,7 +3,10 @@ import { Component } from "@angular/core";
 @Component({
   selector: "bhagavadgita-chapters",
   templateUrl: "./bhagavadgita-chapters.component.html",
-  styleUrls: ["./bhagavadgita-chapters.component.css"]
+  styleUrls: [
+    "./bhagavadgita-chapters.component.scss",
+    "../bhagavadgita.component.scss"
+  ]
 })
 export class BhagavadGitaChaptersComponent {
   title = "This is the Bhagavad Gita Chapters Component";
--- src/app/main/bhagavadgita/bhagavadgita-main/bhagavadgita-main.component.ts
+++ src/app/main/bhagavadgita/bhagavadgita-main/bhagavadgita-main.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "bhagavadgita-main",
   templateUrl: "./bhagavadgita-main.component.html",
-  styleUrls: ["./bhagavadgita-main.component.css"]
+  styleUrls: ["./bhagavadgita-main.component.scss"]
 })
 export class BhagavadGitaMainComponent {
   title = "This is the Bhagavad Gita Main Component";
