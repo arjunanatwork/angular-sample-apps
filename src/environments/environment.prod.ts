869e06a28ab651e8bc91503341ac7c4413092e89
Arjunan Ravi <arjunanatwork@gmail.com>
2019-04-30 00:45:15 +0400

    Config File Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  hackerNewsApiBaseUrl: "http://node-hnapi.herokuapp.com/",
+  pokedexApiBaseUrl: "https://pokeapi.co/api/v2/",
+  bhagavadGitaEndpoints: {
+    token: "https://bhagavadgita.io/auth/oauth/token",
+    api: "https://bhagavadgita.io/api/v1/"
+  }
 };
