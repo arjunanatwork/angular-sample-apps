5fb25c67c0d2f9b534f6ffcf9469bfd79a5e1163
Arjunan Ravi <arjunanatwork@gmail.com>
2019-06-08 01:04:44 +0400

    Commting Prod Prop File

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,17 @@
 export const environment = {
   production: true,
-  hackerNewsApiBaseUrl: "https://node-hnapi.herokuapp.com/",
-  pokedexApiBaseUrl: "https://pokeapi.co/api/v2/",
+  hackerNewsApiBaseUrl: 'https://node-hnapi.herokuapp.com/',
+  pokedexApiBaseUrl: 'https://pokeapi.co/api/v2/',
   bhagavadGitaEndpoints: {
-    token: "https://bhagavadgita.io/auth/oauth/token",
-    api: "https://bhagavadgita.io/api/v1/"
+    token: 'https://bhagavadgita.io/auth/oauth/token',
+    api: 'https://bhagavadgita.io/api/v1/'
+  },
+  firebase: {
+    apiKey: 'AIzaSyDcMC0Nuz32qF3x6Ji3pt8Oq5CQBIBD4vw',
+    authDomain: 'angular-mini-apps.firebaseapp.com',
+    databaseURL: 'https://angular-mini-apps.firebaseio.com',
+    projectId: 'angular-mini-apps',
+    storageBucket: 'angular-mini-apps.appspot.com',
+    messagingSenderId: '277563647174'
   }
 };
